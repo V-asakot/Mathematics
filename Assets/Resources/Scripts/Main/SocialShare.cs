738fb6c56db19f170d479ad9e2ca10dd0d82298e
Kotkov Vasilii <vasily.kotkoff@yandex.ru>
2021-06-05 00:19:15 +0300

    folder names

--- Assets/Resources/Scripts/Main/SocialShare.cs
+++ Assets/Resources/Scripts/Main/SocialShare.cs
@@ -30,40 +30,8 @@ public class SocialShare : MonoBehaviour {
 
         isProcessingShare = true;
 
-        /*  main.ads.HideBanner(true);
-          yield return new WaitForSecondsRealtime(0.5f);
-          yield return new WaitForEndOfFrame();
-          string path = Application.persistentDataPath + "/ScreenShot";
-          if (!Directory.Exists(path))
-          {
-              Directory.CreateDirectory(path);
-
-          }
-          ScreenCapture.CaptureScreenshot("ScreenShot/screenshot.png", 2);
-          string destination = Path.Combine(path, "screenshot.png");
-          yield return new WaitForSecondsRealtime(0.3f);
-
-          if (!Application.isEditor)
-          { 
-
-              AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-              AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
-              intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-              AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
-              AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "file://" + destination);
-              intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
-              intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "Поделитесь игрой с друзьями! ");
-              intentObject.Call<AndroidJavaObject>("setType", "image/jpeg");
-              AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-              AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
-              AndroidJavaObject chooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Поделись с друзьями");
-              currentActivity.Call("startActivity", chooser);
-
-              yield return new WaitForSecondsRealtime(1);
-          }
-          main.ads.HideBanner(false);
-          isProcessingShare = false;
-         */
+        
+        
         yield return new WaitForEndOfFrame();
         Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture();
         ShareSheet share = new ShareSheet();
